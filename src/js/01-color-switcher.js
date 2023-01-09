5c8c8db79af79f1814ff6af60656027ba9827f35
Aleksandra <ag7756862@gmail.com>
2023-01-10 01:27:45 +0200

    feat: add solution task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const refs = {
+  buttonStartEl: document.querySelector("[data-start]"),
+  buttonStopEl: document.querySelector("[data-stop]"),
+}
+let intervalId = null;
+const DELAY = 1000;
+
+refs.buttonStartEl.addEventListener('click', onStartBtnClick)
+refs.buttonStopEl.addEventListener('click', onStopBtnClick)
+
+
+  function onStartBtnClick() {
+    intervalId = setInterval(onButtonStart, DELAY);
+    refs.buttonStartEl.setAttribute('disabled', '');
+    console.log(refs.buttonStartEl);
+  }
+
+  function onStopBtnClick() {
+    clearInterval(intervalId);
+    refs.buttonStartEl.removeAttribute('disabled')
+  }
+
+function onButtonStart(){
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
